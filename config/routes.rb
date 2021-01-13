da131baa0280599f4907839d954c4c21a17dd1cf
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-01-12 21:27:44 -0500

    before heroku deployment working version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create, :show]
+  end
+  resources :doses, only: [:destroy]
 end
